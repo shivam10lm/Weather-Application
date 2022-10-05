45e0189c38469c9260c35a49362984648feaf220
Shivam Gupta <51778763+shivam10lm@users.noreply.github.com>
2022-10-04 21:08:37 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import React from "react";
+import Temp from "./Temp";
+
+const App = () => {
+  return (
+    <>
+      <Temp />
+    </>
+  );
+};
+
+export default App;
---
+++ src/Temp.js
@@ -0,0 +1,71 @@
+//api.openweathermap.org/data/2.5/weather?q={city name}&appid={c23eb7e9119a4948ddbee0dfd49a1320}
+import "./styles.css";
+import React, { useEffect, useState } from "react";
+import Weather from "./Weather";
+
+const Temp = () => {
+  const [searchValue, setSearchValue] = useState("delhi");
+  const [tempInfo, setTempInfo] = useState({});
+
+  const getWeatherInfo = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=c23eb7e9119a4948ddbee0dfd49a1320`;
+
+      const res = await fetch(url);
+      const data = await res.json();
+
+      const { temp, humidity, pressure } = data.main;
+      const { main: weathermood } = data.weather[0];
+      const { name } = data;
+      const { speed } = data.wind;
+      const { country, sunset } = data.sys;
+
+      const myNewWeatherInfo = {
+        temp,
+        humidity,
+        pressure,
+        weathermood,
+        name,
+        speed,
+        country,
+        sunset
+      };
+
+      setTempInfo(myNewWeatherInfo);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getWeatherInfo();
+  }, []);
+  return (
+    <>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="search"
+            placeholder="Search..."
+            autoFocus
+            id="search"
+            className="searchTerm"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+
+          <button
+            className="searchButton"
+            type="button"
+            onClick={getWeatherInfo}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+      <Weather {...tempInfo} />
+    </>
+  );
+};
+
+export default Temp;
